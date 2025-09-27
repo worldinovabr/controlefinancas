b27f7e64414d8ae8e843bef3e01f8e0b1152f37e
Weslei BBS <wesleibbs@gmail.com>
2025-09-27 13:36:02 -0300

    chore: remove tools/ from repo, keep locally

--- tools/push-server/gen-keys.js
+++
@@ -1,9 +0,0 @@
-const webpush = require('web-push');
-const fs = require('fs');
-const path = require('path');
-
-const keys = webpush.generateVAPIDKeys();
-fs.writeFileSync(path.join(__dirname, 'vapid.json'), JSON.stringify(keys, null, 2));
-console.log('VAPID keys generated and saved to vapid.json');
-console.log('Public Key:', keys.publicKey);
-console.log('Private Key:', keys.privateKey);
